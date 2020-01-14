d9d7ae0d451a56a842b8960a5508f485aadf5fb3
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-01-14 16:45:27 -0600

    feature: add API skeleton

---
+++ oliver/api.py
@@ -0,0 +1,11 @@
+def post_workflow():
+    "POST /api/workflows/{version}"
+    raise NotImplementedError()
+
+def post_workflow_batch():
+    "POST /api/workflows/{version}/batch"
+    raise NotImplementedError()
+
+def get_workflow_labels():
+    "GET /api/workflows/{version}/{id}/labels"
+    raise NotImplementedError()
