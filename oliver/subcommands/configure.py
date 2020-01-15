4ccd852082e59eb8fc2a48390bcff31efb1cf499
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-01-14 20:04:09 -0600

    improvement(configure): slightly better parsing of `configure` vars

--- oliver/subcommands/configure.py
+++ oliver/subcommands/configure.py
@@ -1,13 +1,28 @@
-from ..config import read_config, write_config
+from ..config import get_default_config, read_config, write_config
+
+QUESTION_MAPPING = {
+    "cromwell_server": "What is the Cromwell server address",
+    "cromwell_api_version": "What is the Cromwell API version"
+}
+def ask(question, default):
+    return input(f"{question} (default: {default})? ")
 
 def call(args):
-    cromwell_server = input("What is the Cromwell server address? ")
-    cromwell_api_version = input("What is the Cromwell API version? ")
-    config = {
-        "cromwell_server": cromwell_server,
-        "cromwell_api_version": cromwell_api_version
-    }
-    write_config(config)
+    starting_config = get_default_config()
+    starting_config.update(read_config())
+    final_config = {}
+    
+    for k, v in starting_config.items():
+        question = "What is the value for '{k}'"
+        if k in QUESTION_MAPPING:
+            question = QUESTION_MAPPING[k]
+
+        answer = ask(question, v)
+        if answer:
+            v = answer
+        final_config[k] = v
+
+    write_config(final_config)
 
 
 def register_subparser(subparser):
