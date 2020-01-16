429a6792b80072611b98484824445be19fffc73e
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-01-16 15:17:37 -0600

    improvement(status): add filtering by workflow name and id

--- oliver/subcommands/status.py
+++ oliver/subcommands/status.py
@@ -20,7 +20,6 @@ def call(args):
             statuses.append("Succeeded")
     else:
         statuses = ["Running"]
-        
 
     workflows = cromwell.get_workflows_query(includeSubworkflows=False, statuses=statuses)
 
@@ -32,13 +31,21 @@ def call(args):
             "Start": pendulum.parse(w["start"]).to_day_datetime_string() if "start" in w else ""
         } for w in workflows]
 
+    if "workflow_id" in args and args["workflow_id"]:
+        results = list(filter(lambda r: r["Workflow ID"] == args["workflow_id"], results))
+
+    if "workflow_name" in args and args["workflow_name"]:
+        results = list(filter(lambda r: r["Workflow Name"] == args["workflow_name"], results))
+
     if len(results) > 0:
         print(tabulate([r.values() for r in results], headers=results[0].keys(), tablefmt=args['grid_style']))
 
 def register_subparser(subparser):
     subcommand = subparser.add_parser("status", help="Report various statistics about a running Cromwell server.")
     subcommand.add_argument("-a", "--all", dest="show_all_statuses", help="Show jobs in all states, not just 'Running' jobs.", default=False, action="store_true")
-    subcommand.add_argument("-r", "--running", dest="show_running_statuses", help="Show jobs in the 'Running' state.", default=False, action="store_true")
     subcommand.add_argument("-f", "--failed", dest="show_failed_statuses", help="Show jobs in the 'Failed' state.", default=False, action="store_true")
+    subcommand.add_argument("-i", "--workflow-id", type=str, help="Filter by workflow id matching argument.")
+    subcommand.add_argument("-n", "--workflow-name", type=str, help="Filter by workflow name matching argument.")
+    subcommand.add_argument("-r", "--running", dest="show_running_statuses", help="Show jobs in the 'Running' state.", default=False, action="store_true")
     subcommand.add_argument("-s", "--succeeded", dest="show_succeeded_statuses", help="Show jobs in the 'Succeeded' state.", default=False, action="store_true")
     subcommand.add_argument("--grid-style", help="Any valid `tablefmt` for python-tabulate.", default="fancy_grid")
