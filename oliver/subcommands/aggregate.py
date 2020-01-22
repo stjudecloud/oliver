ee88d49d7eea27577b1157f3913166e30ffd2be2
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-01-21 22:14:40 -0600

    feature(aggregate): first draft for aggregate subcommand

--- oliver/subcommands/aggregate.py
+++ oliver/subcommands/aggregate.py
@@ -1,21 +1,21 @@
 import argparse
+import os
 
 from typing import Dict
 
-from .. import api, reporting
+from .. import api, errors, reporting
+from . import outputs as _outputs
 
 
-def get_outputs(
-    cromwell_api: api.CromwellAPI, workflow_id: str, output_prefix: str = None
-):
-    outputs = cromwell_api.get_workflows_outputs(workflow_id)
-    results = [{"Output Name": k, "Location": v} for k, v in outputs["outputs"].items()]
+def process_output(dest_folder: str, output: str):
+    if not output.startswith("s3://"):
+        errors.warn(
+            f"Could not copy {output}. Only s3 buckets are currently supported!",
+            fatal=False,
+            int=errors.ERROR_INVALID_INPUT,
+        )
 
-    if output_prefix:
-        for result in results:
-            result["Location"] = args["output_prefix"] + result["Location"]
-
-    return results
+    return f"aws s3 cp --sse AES256 {output} {dest_folder}"
 
 
 def call(args: Dict):
@@ -28,12 +28,14 @@ def call(args: Dict):
     cromwell = api.CromwellAPI(
         server=args["cromwell_server"], version=args["cromwell_api_version"]
     )
-    results = get_outputs(
+    outputs = _outputs.get_outputs(
         cromwell,
         args["workflow-id"],
         output_prefix=None if "output_prefix" not in args else args["output_prefix"],
     )
-    reporting.print_dicts_as_table(results)
+
+    for output in outputs:
+        os.system(process_output(args["output-folder"], output["Location"]))
 
 
 def register_subparser(subparser: argparse._SubParsersAction):
@@ -44,9 +46,15 @@ def register_subparser(subparser: argparse._SubParsersAction):
     """
 
     subcommand = subparser.add_parser(
-        "outputs", aliases=["o"], help="Find all reported outputs for a given workflow."
+        "aggregate",
+        aliases=["a"],
+        help="Aggregate all results to a local or cloud folder for a run.",
     )
     subcommand.add_argument("workflow-id", help="Cromwell workflow ID.")
+    subcommand.add_argument(
+        "output-folder",
+        help="Output folder to localize the files to (currently supports S3).",
+    )
     subcommand.add_argument(
         "--grid-style",
         help="Any valid `tablefmt` for python-tabulate.",
