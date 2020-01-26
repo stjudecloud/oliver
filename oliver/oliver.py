cb4f7749df2b6c9b594d404d911e910f8ae15366
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-01-26 15:17:07 -0600

    improve: when name or group is not set, return '<not set>'

--- oliver/oliver.py
+++ oliver/oliver.py
@@ -1,7 +1,6 @@
-
 from typing import Dict, Optional
 
-from . import constants 
+from . import constants
 
 
 def get_oliver_name(workflow: Dict) -> Optional[str]:
@@ -14,7 +13,7 @@ def get_oliver_name(workflow: Dict) -> Optional[str]:
     if constants.OLIVER_JOB_NAME_KEY in workflow.get("labels", {}):
         return workflow.get("labels", {}).get(constants.OLIVER_JOB_NAME_KEY)
 
-    return None
+    return "<not set>"
 
 
 def get_oliver_group(workflow: Dict) -> Optional[str]:
@@ -27,4 +26,4 @@ def get_oliver_group(workflow: Dict) -> Optional[str]:
     if constants.OLIVER_JOB_GROUP_KEY in workflow.get("labels", {}):
         return workflow.get("labels", {}).get(constants.OLIVER_JOB_GROUP_KEY)
 
-    return None
+    return "<not set>"
