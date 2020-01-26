afa47fbf26e81478a36e8d83331e21fd1c6fb251
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-01-25 20:05:26 -0600

    improve: generalize batching code so it's easily used in other pieces of the code

--- oliver/batch.py
+++ oliver/batch.py
@@ -1,8 +1,55 @@
 import pendulum
 
-from typing import List, Dict
+from logzero import logger
+from typing import Dict, List, Union
 
 from . import errors
+
+
+def get_workflow_batches(
+    workflows: List[dict],
+    batches: Union[int, List[int]],
+    batch_interval_mins: int = 5,
+    relative: bool = False,
+):
+    """Returns all workflows where the derived batch number is included in `batches`.
+    
+    args:
+        workflows (List[dict]): list of workflows returned from cromwell. must
+                                contain the `submission` key.
+        batches(Union[int, List[int]]): batches to be returned. this can be either
+                                        a single integer or a list of integers describing
+                                        the batch numbers.
+        batch_interval_mins (int, optional): interval to constitute a new batch 
+                                             in minutes. defaults to 5.
+        relative(bool): if True, batches will be considered as "N batches ago" (e.g.
+                        `0` will return the most recent batch, `1` will return the second
+                        most recent batch).
+    returns:
+        List[dict]: the `workflows` filtered by batch number.
+    """
+
+    if isinstance(batches, int):
+        batches = [batches]
+
+    _workflows, _max_batch_num = batch_workflows(
+        workflows, batch_interval_mins=batch_interval_mins
+    )
+    _batches = batches
+
+    if relative:
+        _batches = [_max_batch_num - b for b in _batches]
+        if any([b < 0 for b in _batches]):
+            errors.report(
+                message="One of the batch numbers given was higher than the largest batch number, so it does not exist!",
+                fatal=True,
+                exitcode=errors.ERROR_INVALID_INPUT,
+            )
+
+    logger.info(
+        f"Targetting all jobs in batch(es): {', '.join([str(b) for b in _batches])} (original={', '.join([str(b) for b in batches])}, relative={relative})."
+    )
+    return list(filter(lambda w: w.get("batch") in _batches, _workflows))
 
 
 def batch_workflows(workflows: List[Dict], batch_interval_mins: int = 5) -> List[Dict]:
@@ -21,15 +68,18 @@ def batch_workflows(workflows: List[Dict], batch_interval_mins: int = 5) -> List
                                              in minutes. Defaults to 5.
     
     Returns:
-        List[Dict]: the `workflows` list that was passed in, but with a new 
-                    `batch` key added to each entry denoting the computed batch.
+        List[Dict], int: The first returned value is the `workflows` list that was 
+                         passed in, but with a new `batch` key added to each entry 
+                         denoting the computed batch. The second returned value is the
+                         maximum batch number.
     """
 
     batch_num = 0
     last_submission_time = None
     for i in range(len(workflows)):
         w = workflows[i]
-        if not w.get("submission"):
+        submission = w.get("submission")
+        if not submission:
             errors.report(
                 f"`submission` not in workflow, so we cannot batch.\n{w}",
                 fatal=True,
@@ -37,8 +87,7 @@ def batch_workflows(workflows: List[Dict], batch_interval_mins: int = 5) -> List
                 suggest_report=True,
             )
 
-        w = workflows[i]
-        t = pendulum.parse(w["submission"])
+        t = pendulum.parse(submission)
         if last_submission_time:
             delta = (t - last_submission_time).total_minutes()
             if delta > batch_interval_mins:
@@ -46,4 +95,4 @@ def batch_workflows(workflows: List[Dict], batch_interval_mins: int = 5) -> List
         workflows[i]["batch"] = batch_num
         last_submission_time = t
 
-    return workflows
+    return workflows, batch_num
