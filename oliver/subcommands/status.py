ee7107f848ed61ed05daadb4e6460e4a213c88b1
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-01-30 22:46:01 -0600

    improve: generalize batches argument group

--- oliver/subcommands/status.py
+++ oliver/subcommands/status.py
@@ -8,6 +8,7 @@ from typing import Dict, List, Optional
 
 from ..lib import (
     api,
+    args as _args,
     batch,
     constants,
     errors,
@@ -113,29 +114,7 @@ def register_subparser(subparser: argparse._SubParsersAction):
         default=False,
         action="store_true",
     )
-    batches = subcommand.add_mutually_exclusive_group()
-    batches.add_argument(
-        "-b",
-        "--batches-relative",
-        help="Starting with the _most recent_ batch, compute batches separated by `batch-interval-mins`. Any batches not contained in `batches` are filtered.",
-        default=None,
-        nargs="+",
-        type=int,
-    )
-    batches.add_argument(
-        "-B",
-        "--batches-absolute",
-        help="Starting with the _first batch in time_, compute batches separated by `batch-interval-mins`. Any batches not contained in `batches` are filtered.",
-        nargs="+",
-        default=None,
-        type=int,
-    )
-    subcommand.add_argument(
-        "-x",
-        "--batch-interval-mins",
-        help="Split batches by any two jobs separated by N minutes.",
-        type=int,
-    )
+    _args.add_batches_group(subcommand)
     subcommand.add_argument(
         "--failed-calls",
         help="Only show calls with the given name that failed (useful in restarting failed steps).",
