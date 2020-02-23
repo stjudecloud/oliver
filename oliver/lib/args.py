af1b4709d339e0ab5f1f00b934ac7f2930a4b6b8
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-02-22 18:47:02 -0600

    improve: add --version argument

--- oliver/lib/args.py
+++ oliver/lib/args.py
@@ -1,10 +1,21 @@
+def add_version_arg(parser, required=False):
+    """adds version arguments to a parser."""
+
+    parser.add_argument(
+        "--version",
+        help="Displays the current version of the tool.",
+        default=False,
+        action="store_true",
+    )
+
+
 def add_loglevel_group(parser, required=False):
     """Adds log level groups to a parser."""
 
     loglevel_group = parser.add_mutually_exclusive_group(required=required)
     loglevel_group.add_argument(
         "--debug",
-        help="Set the log level to DEBUG.",
+        help="Set the log level to debug.",
         default=False,
         action="store_true",
     )
