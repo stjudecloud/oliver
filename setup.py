5a3037f06c9b44e198844886db0c5684835a1105
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-02-22 18:39:27 -0600

    improve: automated releases using python-semantic-release

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python
 
+import os
+import re
+
 from setuptools import setup, find_packages
 
-__VERSION__ = "1.1.0"
+root_dir = os.path.abspath(os.path.dirname(__file__))
 
-with open("requirements.txt", encoding="utf-8") as f:
+with open(os.path.join(root_dir, "oliver", "__init__.py"), mode="r", encoding="utf-8") as fd:
+    version = re.search(
+        r"^__version__\s*=\s*[\"']([^\"']*)[\"']",
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
+with open("requirements.txt", mode="r", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.md", mode="r", encoding="utf-8") as fh:
     readme = fh.read()
 
 setup(
     name="stjudecloud-oliver",
-    version=__VERSION__,
+    version=version,
     description="An opinionated Cromwell orchestration system",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="St. Jude Cloud Team",
     author_email="support@stjude.cloud",
-    scripts=["scripts/oliver"],
+    scripts=["bin/oliver"],
     packages=find_packages(),
     install_requires=requirements,
     python_requires=">=3.0, <3.8",
